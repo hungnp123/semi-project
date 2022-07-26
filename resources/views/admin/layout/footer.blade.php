973457658a04606999be7456b9acefbf969ed187
hungnp123 <105684834+hungnp123@users.noreply.github.com>
2022-07-26 10:47:33 +0700

    Create footer.blade.php

---
+++ resources/views/admin/layout/footer.blade.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home page</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="footer">
+        <br>
+        <hr>
+        <img style="float: right;" src="https://images.milledcdn.com/2018-04-24/7ZkLhOzxOExjNvwm/9MOJZopZWnhy.jpg" width="300px"; height="50px">
+        <p>Information:</p>
+        <ul>
+            <li><p>Address: Hanoi,Vietnam</p></li>
+            <li><p>Phone: 01234</p></li>
+            <li><p>Email: RLhouse@gmail.com</p></li>
+        </ul>
+        <hr>
+            <br>
+            <p style="text-align: center;">© COPYRIGHT 2020 RALPH LAUREN. ALL RIGHTS RESERVED.</p>
+            <br>
+    </div>
+</body>       
+</html>
