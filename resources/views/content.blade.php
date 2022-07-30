a5c42977837af1ea4400810e7a8aff913ef27141
hungnp123 <105684834+hungnp123@users.noreply.github.com>
2022-07-30 16:02:06 +0700

    create club by Phi Hung

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -8,54 +8,51 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <style type="text/css">
-    .header{
-    	
+    .club{
+        margin-bottom: 30px;
+    	height: auto;
+    	width: 25%;
+        text-align: center;
+        padding-left: 20px;
+        padding-top: 8px;
+        padding-right: 20px;
+        padding-bottom: 10px;
+    	border-radius: 2%;
+    	margin-left: 30px;
+        margin-top: 30px;
+    	margin-right: 15px;
+    	box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+ 		background: white;
     }
-    .header ul li{
-    	list-style: none;
-    }
-    .header img{
-    	float:left;
-    }
-	.header a{
-		margin-top: 15px;
-		margin-right: 10px;
-		margin-left: 10px;
-		margin-bottom: 15px;
-	}
 </style>
 <body>
     <div class="wrapper">
         <div class="club">
-<table border="1 solid black">
-    <tr> 
-        <th>Club Name: </th>
-        <th>Images: </th>
-        <th>Country: </th>
-
-        <th>Action: </th>
-     </tr> 
-        <?php 
-        $connect = mysqli_connect('localhost','root','','semi_project');
-        $sql = "SELECT * FROM club";
-        $result = mysqli_query($connect, $sql);
-        while($row= mysqli_fetch_array($result)){
-            $club_id = $row['club_id'];
-            $club_name = $row['club_name'];
-            $club_img = $row['club_img'];
-            $club_national = $row['club_national'];
-            ?>
-        <tr>
-            <td> <?php echo $club_name ?></td>
-            <td> <img src="img/<?php echo $club_img ?>" style ="width: 80px; height: 80px;"></td>
-            <td> <?php echo $club_national ?></td>
-            <td> <a href='detail.php ?id=<?php echo $club_id; ?>' class='btn btn-primary'>Details</a></td>
-        </tr>
-            <?php
-            }
-            ?>      
-</table>
+            <table>
+                <h2>Club</h2>
+                <hr/>
+                <?php 
+                    $connect = mysqli_connect('localhost','root','','semi_project');
+                    $sql = "SELECT * FROM club";
+                    $result = mysqli_query($connect, $sql);
+                    while($row= mysqli_fetch_array($result)){
+                        $club_id = $row['club_id'];
+                        $club_name = $row['club_name'];
+                        $club_img = $row['club_img'];
+                        $club_national = $row['club_national'];
+                ?>
+                <tr>
+                    <td> <?php echo $club_name ?></td>
+                    <td> <img src="img/<?php echo $club_img ?>" style ="width: 80px; height: 80px;"></td>
+                    <td> <?php echo $club_national ?></td>
+                    <td> <a href='detail.php ?id=<?php echo $club_id; ?>' class='btn btn-primary'>Details</a></td>
+                </tr>
+                <?php
+                    }
+                ?>      
+            </table>
         </div>
+
     </div>
 </body>       
 </html>
