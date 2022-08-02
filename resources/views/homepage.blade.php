0c78a16705e50d8ac721877cf7b1fba7e016e612
hungnp123 <105684834+hungnp123@users.noreply.github.com>
2022-08-02 08:10:38 +0700

    Update homepage.blade.php

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,7 +1,10 @@
 @include('admin.layout.header');
 <div class = "navbar">
 @include('admin.layout.banner');    
-<div>
+</div>
+<div class = "content">
+    @include('content');
+</div>
 <div class = "footer"> 
     @include('admin.layout.footer');
     </div>
